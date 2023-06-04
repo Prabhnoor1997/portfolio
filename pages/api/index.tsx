4a94ddeadc35858e26fe6016eeb4819d61330596
Prabhnoor <prabhnoor.parry@gmail.com>
2023-06-04 17:23:33 -0400

    first base

---
+++ pages/api/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from "next/head";
+//import styles from "../styles/Ho"
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title> Parry's Portfolio</title>
+            </Head>
+            <h1 className = "text-red-500">Lets Build</h1>
+        </div>
+    );
+};
+export default Home 
